ba1a93a3b2c9e7d5eab35e41e049bdea7baf3566
Andreas Hartl <combine-harvester@nd-com.net>
2019-06-17 09:29:55 +0200

    Added Empty publisher

---
+++ Tests/CombineHarvesterTests/EmptyTests.swift
@@ -0,0 +1,31 @@
+//
+//  EmptyTests.swift
+//  CombineHarvesterTests
+//
+//  Created by Andreas Hartl on 17.06.19.
+//
+
+import XCTest
+@testable import CombineHarvester
+
+class EmptyTests: XCTestCase {
+    func testCompleteImmediately() {
+        let subject = TestSubject()
+
+        let underTest = Publishers.Empty(completeImmediately: true, outputType: String.self, failureType: Never.self)
+        _ = underTest.subscribe(subject)
+
+        XCTAssertEqual(subject.values, [])
+        XCTAssertEqual(subject.completion, [.finished])
+    }
+
+    func testDoNotCompleteImmediately() {
+        let subject = TestSubject()
+
+        let underTest = Publishers.Empty(completeImmediately: false, outputType: String.self, failureType: Never.self)
+        _ = underTest.subscribe(subject)
+
+        XCTAssertEqual(subject.values, [])
+        XCTAssertEqual(subject.completion, [])
+    }
+}
