4db9b9e73b47ffd616a89c805a3dae50eb555fb5
Andreas Hartl <combine-harvester@nd-com.net>
2019-06-25 17:03:15 +0200

    Added AllSatisfy publisher

---
+++ Sources/CombineHarvester/AllSatisfy.swift
@@ -0,0 +1,110 @@
+
+extension Publishers {
+    /// A publisher that publishes a single Boolean value that indicates whether all received elements pass a given predicate.
+    public struct AllSatisfy<Upstream>: Publisher where Upstream: Publisher {
+        public typealias Output = Bool
+        public typealias Failure = Upstream.Failure
+
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+
+        /// A closure that evaluates each received element.
+        ///
+        ///  Return `true` to continue, or `false` to cancel the upstream and finish.
+        public let predicate: (Upstream.Output) -> Bool
+
+        public func receive<S>(subscriber: S) where S: Subscriber, Upstream.Failure == S.Failure, S.Input == Publishers.AllSatisfy<Upstream>.Output {
+            var didRequest = false
+            self.upstream.subscribe(TransformingSubscriber(
+                subscriber: subscriber,
+                transformRequest: { demand in
+                    guard demand > .none, !didRequest else {
+                        return [.demand(.none)]
+                    }
+                    didRequest = true
+                    return [.demand(.unlimited)]
+                }, transformValue: { value in
+                    if !self.predicate(value) {
+                        return [.value(false), .finished]
+                    }
+                    return [.demand(.unlimited)]
+                }, transformCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        return [.value(true), .finished]
+                    case let .failure(error):
+                        return [.failure(error)]
+                    }
+                }
+            ))
+        }
+    }
+
+    /// A publisher that publishes a single Boolean value that indicates whether all received elements pass a given error-throwing predicate.
+    public struct TryAllSatisfy<Upstream>: Publisher where Upstream: Publisher {
+        public typealias Output = Bool
+        public typealias Failure = Error
+
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+
+        /// A closure that evaluates each received element.
+        ///
+        /// Return `true` to continue, or `false` to cancel the upstream and complete. The closure may throw, in which case the publisher cancels the upstream publisher and fails with the thrown error.
+        public let predicate: (Upstream.Output) throws -> Bool
+
+        public func receive<S>(subscriber: S) where S: Subscriber, S.Failure == Publishers.TryAllSatisfy<Upstream>.Failure, S.Input == Publishers.TryAllSatisfy<Upstream>.Output {
+            var didRequest = false
+            self.upstream.subscribe(TransformingSubscriber(
+                subscriber: subscriber,
+                transformRequest: { demand in
+                    guard demand > .none, !didRequest else {
+                        return [.demand(.none)]
+                    }
+                    didRequest = true
+                    return [.demand(.unlimited)]
+                }, transformValue: { value in
+                    do {
+                        if try !self.predicate(value) {
+                            return [.value(false), .finished]
+                        }
+                        return [.demand(.unlimited)]
+                    } catch {
+                        return [.failure(error)]
+                    }
+                }, transformCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        return [.value(true), .finished]
+                    case let .failure(error):
+                        return [.failure(error)]
+                    }
+                }
+            ))
+        }
+    }
+}
+
+extension Publisher {
+    /// Publishes a single Boolean value that indicates whether all received elements pass a given predicate.
+    ///
+    /// When this publisher receives an element, it runs the predicate against the element. If the predicate returns `false`, the publisher produces a `false` value and finishes. If the upstream publisher finishes normally, this publisher produces a `true` value and finishes.
+    /// As a `reduce`-style operator, this publisher produces at most one value.
+    /// Backpressure note: Upon receiving any request greater than zero, this publisher requests unlimited elements from the upstream publisher.
+    /// - Parameter predicate: A closure that evaluates each received element. Return `true` to continue, or `false` to cancel the upstream and complete.
+    /// - Returns: A publisher that publishes a Boolean value that indicates whether all received elements pass a given predicate.
+    public func allSatisfy(_ predicate: @escaping (Self.Output) -> Bool) -> Publishers.AllSatisfy<Self> {
+        return Publishers.AllSatisfy(upstream: self, predicate: predicate)
+    }
+
+    /// Publishes a single Boolean value that indicates whether all received elements pass a given error-throwing predicate.
+    ///
+    /// When this publisher receives an element, it runs the predicate against the element. If the predicate returns `false`, the publisher produces a `false` value and finishes. If the upstream publisher finishes normally, this publisher produces a `true` value and finishes. If the predicate throws an error, the publisher fails, passing the error to its downstream.
+    /// As a `reduce`-style operator, this publisher produces at most one value.
+    /// Backpressure note: Upon receiving any request greater than zero, this publisher requests unlimited elements from the upstream publisher.
+    /// - Parameter predicate:  A closure that evaluates each received element. Return `true` to continue, or `false` to cancel the upstream and complete. The closure may throw, in which case the publisher cancels the upstream publisher and fails with the thrown error.
+    /// - Returns:  A publisher that publishes a Boolean value that indicates whether all received elements pass a given predicate.
+    public func tryAllSatisfy(_ predicate: @escaping (Self.Output) throws -> Bool) -> Publishers.TryAllSatisfy<Self> {
+        return Publishers.TryAllSatisfy(upstream: self, predicate: predicate)
+    }
+}
---
+++ Tests/CombineHarvesterTests/AllSatisfyTests.swift
@@ -0,0 +1,75 @@
+//
+//  AllSatisfyTests.swift
+//  CombineHarvesterTests
+//
+//  Created by Andreas Hartl on 25.06.19.
+//
+
+import XCTest
+
+class AllSatisfyTests: XCTestCase {
+    func testAllSatisfy() {
+        let lorem = ["Lorem", "ipsum", "dolor", "sit", "amet"]
+        let tests = (0..<lorem.count).map { Array(lorem[..<$0]) }
+        var publisher: TestPublisher<String, TestError>
+        var receiver: TestSubject<Bool, TestError>
+
+        for sequence in tests {
+            publisher = TestPublisher.success(sequence)
+            receiver = TestSubject()
+            _ = publisher.allSatisfy { !$0.isEmpty }.subscribe(receiver)
+
+            XCTAssertEqual(receiver.values, [true])
+            XCTAssertEqual(receiver.completion, [.finished])
+
+            publisher = TestPublisher(sequence.map(Result.success) + [.failure(.error)])
+            receiver = TestSubject()
+            _ = publisher.allSatisfy { !$0.isEmpty }.subscribe(receiver)
+
+            XCTAssertEqual(receiver.values, [])
+            XCTAssertEqual(receiver.completion, [.failure(.error)])
+
+            publisher = TestPublisher.success(sequence)
+            receiver = TestSubject()
+            _ = publisher.allSatisfy { $0 != "dolor" }.subscribe(receiver)
+
+            XCTAssertEqual(receiver.values, [sequence.count <= 2 ? true : false])
+            XCTAssertEqual(receiver.completion, [.finished])
+
+            publisher = TestPublisher(sequence.map(Result.success) + [.failure(.error)])
+            receiver = TestSubject()
+            _ = publisher.allSatisfy { $0 != "dolor" }.subscribe(receiver)
+
+            XCTAssertEqual(receiver.values, sequence.count <= 2 ? [] : [false])
+            XCTAssertEqual(receiver.completion, [sequence.count <= 2 ? .failure(.error) : .finished])
+
+            publisher = TestPublisher.success(sequence)
+            receiver = TestSubject()
+            _ = publisher
+                .tryAllSatisfy { if $0 == "dolor" {
+                    throw TestError.otherError
+                }
+                return true
+                }
+                .mapError { $0 as! TestError }
+                .subscribe(receiver)
+
+            XCTAssertEqual(receiver.values, sequence.count <= 2 ? [true] : [])
+            XCTAssertEqual(receiver.completion, sequence.count <= 2 ? [.finished] : [.failure(.otherError)])
+
+            publisher = TestPublisher(sequence.map(Result.success) + [.failure(.error)])
+            receiver = TestSubject()
+            _ = publisher
+                .tryAllSatisfy { if $0 == "dolor" {
+                    throw TestError.otherError
+                }
+                return true
+                }
+                .mapError { $0 as! TestError }
+                .subscribe(receiver)
+
+            XCTAssertEqual(receiver.values, [])
+            XCTAssertEqual(receiver.completion, sequence.count <= 2 ? [.failure(.error)] : [.failure(.otherError)])
+        }
+    }
+}
